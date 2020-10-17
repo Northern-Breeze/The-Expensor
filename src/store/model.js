4cda3fa3aad5353ffcf49d797ddcf3c6fb018570
Thagana <samuelmthwa79@gmail.com>
2020-10-18 00:02:39 +0200

    store

--- src/store/model.js
+++ src/store/model.js
@@ -1,3 +1,8 @@
+import { action } from "easy-peasy";
+
 export default {
-    currentBalance: 0
+    currentBalance: 0,
+    setCurrentBalance: action((state, payload) => {
+        state.currentBalance= payload;
+    })
 }
