35cb1d2ce2ca02db69bc33b921ed181272bdba46
Thagana <samuelmthwa79@gmail.com>
2020-10-17 16:38:44 +0200

    entry point file

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import Routes from './src/routes/Routes';
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <NavigationContainer>
+        <Routes />
+    </NavigationContainer>
   )
 }
