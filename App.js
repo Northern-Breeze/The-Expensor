bb297d1a45354ec536381c84fcd29fb609219bca
Thagana <samuelmthwa79@gmail.com>
2020-10-17 23:40:25 +0200

    store link

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from './src/routes/Routes';
+import { createStore, StoreProvider as Provider } from 'easy-peasy';
+import Store from './src/store/model';
+
+const store = createStore(Store);
 
 export default function App() {
   return (
-    <NavigationContainer>
-        <Routes />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+          <Routes />
+      </NavigationContainer>
+    </Provider>
   )
 }
