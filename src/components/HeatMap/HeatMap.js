f5d49fce2b7798470b8d68b43734f62017498e83
Thagana <samuelmthwa79@gmail>
2021-06-20 22:12:36 +0200

    fix heatmap barchart and pie chart

--- src/components/HeatMap/HeatMap.js
+++ src/components/HeatMap/HeatMap.js
@@ -33,18 +33,22 @@ export default function HeatMap(props) {
 
     const value = counter(temp);
     setHeat(value);
-  }, []);
+  }, [data]);
   function formatDate() {
     var d = new Date(),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
 
-    if (month.length < 2) month = '0' + month;
-    if (day.length < 2) day = '0' + day;
+    if (month.length < 2) {
+      month = '0' + month;
+    }
+    if (day.length < 2) {
+      day = '0' + day;
+    }
 
     return [year, month, day].join('-');
-}
+  }
   return (
     <ContributionGraph
       values={heat}
